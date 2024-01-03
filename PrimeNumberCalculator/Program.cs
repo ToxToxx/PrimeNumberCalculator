ab9ffa866a410153e0132f481a5f5e650756c754
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2024-01-03 14:17:55 +0300

    Initial commit

---
+++ PrimeNumberCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number;
+
+Console.Write("Введите число");
+number = Convert.ToInt32(Console.ReadLine());
+
+int divisors = 0;
+
+for (int i = 1; i <= number; i++)
+{
+    if (number%i == 0)
+    {
+        divisors++;
+    }
+}
